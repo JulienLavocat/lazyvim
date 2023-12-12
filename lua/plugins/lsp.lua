3ad3bccbff2e8443b15c4b58b6c86ce58acc0bb9
Julien Lavocat <julien.lavocat@gmail.com>
2023-12-12 02:47:47 +0100

    refactor: split base.lua into one file per plugin

---
+++ lua/plugins/lsp.lua
@@ -0,0 +1,16 @@
+return {
+  {
+    "neovim/nvim-lspconfig",
+    ---@class PluginLspOpts
+    opts = {
+      ---@type lspconfig.options
+      servers = {
+        -- pyright will be automatically installed with mason and loaded with lspconfig
+        pyright = {},
+      },
+    },
+  },
+
+  -- for typescript, LazyVim also includes extra specs to properly setup lspconfig, treesitter, mason and typescript.nvim.
+  { import = "lazyvim.plugins.extras.lang.typescript" },
+}
