1d14692c62d02d59be6816b81d2ec19eab7b5e3c
Julien Lavocat <julien.lavocat@gmail.com>
2024-02-24 17:19:37 +0100

    feat: I did somthing with cmp ?

--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -1,97 +1,98 @@
-return {
-  {
-    "hrsh7th/nvim-cmp",
-    dependencies = {
-      "L3MON4D3/LuaSnip",
-      "hrsh7th/cmp-emoji",
-      "hrsh7th/cmp-nvim-lsp",
-      "hrsh7th/cmp-buffer",
-      "hrsh7th/cmp-path",
-      "hrsh7th/cmp-cmdline",
-    },
-    opts = function(_, opts)
-      vim.api.nvim_set_hl(0, "CmpGhostText", { link = "Comment", default = true })
-
-      local cmp = require("cmp")
-
-      table.insert(opts.sources, { name = "emoji" })
-
-      local defaults = require("cmp.config.default")()
-
-      return {
-        snippet = opts.snippet,
-        completion = {
-          completeopt = "menu,menuone,noinsert",
-        },
-        mapping = cmp.mapping.preset.insert({
-          ["<C-n>"] = cmp.mapping.select_next_item({ behavior = cmp.SelectBehavior.Insert }),
-          ["<C-p>"] = cmp.mapping.select_prev_item({ behavior = cmp.SelectBehavior.Insert }),
-          ["<C-b>"] = cmp.mapping.scroll_docs(-4),
-          ["<C-f>"] = cmp.mapping.scroll_docs(4),
-          ["<C-Space>"] = cmp.mapping.complete(),
-          ["<C-e>"] = cmp.mapping.abort(),
-          ["<CR>"] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
-          ["<S-CR>"] = cmp.mapping.confirm({
-            behavior = cmp.ConfirmBehavior.Replace,
-            select = true,
-          }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
-          ["<C-CR>"] = function(fallback)
-            cmp.abort()
-            fallback()
-          end,
-        }),
-        sources = cmp.config.sources({
-          { name = "nvim_lsp" },
-          { name = "path" },
-        }, {
-          { name = "buffer" },
-        }),
-        formatting = {
-          format = function(_, item)
-            local icons = require("lazyvim.config").icons.kinds
-            if icons[item.kind] then
-              item.kind = icons[item.kind] .. item.kind
-            end
-            return item
-          end,
-        },
-        experimental = {
-          ghost_text = {
-            hl_group = "CmpGhostText",
-          },
-        },
-        sorting = defaults.sorting,
-      }
-    end,
-    config = function(_, opts)
-      local cmp = require("cmp")
-
-      cmp.setup.cmdline("/", {
-        mapping = cmp.mapping.preset.cmdline(),
-        sources = {
-          { name = "buffer" },
-        },
-      })
-
-      cmp.setup.cmdline(":", {
-        mapping = cmp.mapping.preset.cmdline(),
-        sources = cmp.config.sources({
-          { name = "path" },
-        }, {
-          {
-            name = "cmdline",
-            option = {
-              ignore_cmds = { "Man", "!" },
-            },
-          },
-        }),
-      })
-
-      for _, source in ipairs(opts.sources) do
-        source.group_index = source.group_index or 1
-      end
-
-      cmp.setup(opts)
-    end,
-  },
-}
+return {}
+-- return {
+--   {
+--     "hrsh7th/nvim-cmp",
+--     dependencies = {
+--       "L3MON4D3/LuaSnip",
+--       "hrsh7th/cmp-emoji",
+--       "hrsh7th/cmp-nvim-lsp",
+--       "hrsh7th/cmp-buffer",
+--       "hrsh7th/cmp-path",
+--       "hrsh7th/cmp-cmdline",
+--     },
+--     opts = function(_, opts)
+--       vim.api.nvim_set_hl(0, "CmpGhostText", { link = "Comment", default = true })
+--
+--       local cmp = require("cmp")
+--
+--       table.insert(opts.sources, { name = "emoji" })
+--
+--       local defaults = require("cmp.config.default")()
+--
+--       return {
+--         snippet = opts.snippet,
+--         completion = {
+--           completeopt = "menu,menuone,noinsert",
+--         },
+--         mapping = cmp.mapping.preset.insert({
+--           ["<C-n>"] = cmp.mapping.select_next_item({ behavior = cmp.SelectBehavior.Insert }),
+--           ["<C-p>"] = cmp.mapping.select_prev_item({ behavior = cmp.SelectBehavior.Insert }),
+--           ["<C-b>"] = cmp.mapping.scroll_docs(-4),
+--           ["<C-f>"] = cmp.mapping.scroll_docs(4),
+--           ["<C-Space>"] = cmp.mapping.complete(),
+--           ["<C-e>"] = cmp.mapping.abort(),
+--           ["<CR>"] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+--           ["<S-CR>"] = cmp.mapping.confirm({
+--             behavior = cmp.ConfirmBehavior.Replace,
+--             select = true,
+--           }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+--           ["<C-CR>"] = function(fallback)
+--             cmp.abort()
+--             fallback()
+--           end,
+--         }),
+--         sources = cmp.config.sources({
+--           { name = "nvim_lsp" },
+--           { name = "path" },
+--         }, {
+--           { name = "buffer" },
+--         }),
+--         formatting = {
+--           format = function(_, item)
+--             local icons = require("lazyvim.config").icons.kinds
+--             if icons[item.kind] then
+--               item.kind = icons[item.kind] .. item.kind
+--             end
+--             return item
+--           end,
+--         },
+--         experimental = {
+--           ghost_text = {
+--             hl_group = "CmpGhostText",
+--           },
+--         },
+--         sorting = defaults.sorting,
+--       }
+--     end,
+--     config = function(_, opts)
+--       local cmp = require("cmp")
+--
+--       cmp.setup.cmdline("/", {
+--         mapping = cmp.mapping.preset.cmdline(),
+--         sources = {
+--           { name = "buffer" },
+--         },
+--       })
+--
+--       cmp.setup.cmdline(":", {
+--         mapping = cmp.mapping.preset.cmdline(),
+--         sources = cmp.config.sources({
+--           { name = "path" },
+--         }, {
+--           {
+--             name = "cmdline",
+--             option = {
+--               ignore_cmds = { "Man", "!" },
+--             },
+--           },
+--         }),
+--       })
+--
+--       for _, source in ipairs(opts.sources) do
+--         source.group_index = source.group_index or 1
+--       end
+--
+--       cmp.setup(opts)
+--     end,
+--   },
+-- }
