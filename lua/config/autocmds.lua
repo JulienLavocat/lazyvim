2b980e48dc787e88b823a4bf064bafb7eb763365
Julien Lavocat <julien.lavocat@gmail.com>
2024-02-24 17:59:22 +0100

    feat: open neotree on startup

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -1,3 +1,8 @@
 -- Autocmds are automatically loaded on the VeryLazy event
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 -- Add any additional autocmds here
+
+-- Open neotree on startup
+if vim.fn.argc(-1) == 0 then
+  vim.cmd("Neotree show")
+end
