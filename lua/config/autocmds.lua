550972165ac6de4abeb577c92e93eaa7c8dc12f2
Julien Lavocat <julien.lavocat@gmail.com>
2024-02-24 18:03:14 +0100

    docs: improve neotree autoopen comment

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -2,7 +2,7 @@
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 -- Add any additional autocmds here
 
--- Open neotree on startup
+-- Open neotree on startup when there are no arguments (plays nicely with auto-sessions)
 if vim.fn.argc(-1) == 0 then
   vim.cmd("Neotree show")
 end
