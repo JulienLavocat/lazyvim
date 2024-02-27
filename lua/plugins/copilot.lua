63aaad9a442f98fb6ff4e5d41556be3239d40e92
Julien Lavocat <julien.lavocat@gmail.com>
2024-02-27 12:01:38 +0100

    feat: bring back copilot (but fow how long ? :D)

---
+++ lua/plugins/copilot.lua
@@ -0,0 +1,15 @@
+return {
+  {
+    "zbirenbaum/copilot.lua",
+    config = function()
+      require("copilot").setup({
+        suggestion = {
+          auto_trigger = true,
+        },
+        filetypes = {
+          markdown = true,
+        },
+      })
+    end,
+  },
+}
