851e84a59fb7ce118bcbf56e0c349ae41c3445d8
Julien Lavocat <julien.lavocat@gmail.com>
2024-07-08 10:48:07 +0200

    feat: gruvbox my beloved

---
+++ lua/plugins/theme.lua
@@ -0,0 +1,11 @@
+return {
+  { "wittyjudge/gruvbox-material.nvim", opts = {
+    style = "dark",
+  } },
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "gruvbox-material",
+    },
+  },
+}
